13c8d8f46c4c59eb2ddd2e01d808e94cd13f1615
iwmstjp <miwaura95@gmail.com>
2023-07-05 10:13:32 +0900

    add flex and jQuery

--- 2023/jQuery/index.js
+++ 2023/jQuery/index.js
@@ -1,1 +1,11 @@
-$("h1").css("color", "red");
+$("h1").addClass("big-title");
+$("button").click(function(){
+    // $("h1").css("color", "blue");
+    $("h1").hide();
+})
+$("body").keydown(function(event){
+    // $("h1").html(event.key);
+    // console.log(event.key);
+    $("h1").show();
+})
+$("h1").before("<button>New</button>");
